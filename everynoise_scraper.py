5ff5d2c436e5db76d5e00f118f0ec4230b40bff0
Bas Ekkelenkamp <basekkelenkamp@MacBook-Pro.local>
2022-11-27 20:18:44 +0100

    genre guessing game

--- everynoise_scraper.py
+++ everynoise_scraper.py
@@ -3,10 +3,13 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 from random import randint
-
 from pandas import Series
+import wordsegment
+
+wordsegment.load()
 
 base_url = "https://everynoise.com/"
+game_length = 5
 
 
 def scrape_all_genres():
@@ -43,14 +46,16 @@ def scrape_genre_artists(genre: Series):
     canvas = soup.find("div", {"class": "canvas"})
     artist_divs = canvas.find_all("div")
 
-    return pd.DataFrame([
-        {
-            "artist": artist_div.text.replace("» ", ""),
-            "preview_url": artist_div.get("preview_url"),
-            "spotify_link": artist_div.a.get("href"),
-        }
-        for artist_div in artist_divs
-    ])
+    return pd.DataFrame(
+        [
+            {
+                "artist": artist_div.text.replace("» ", ""),
+                "preview_url": artist_div.get("preview_url"),
+                "spotify_link": artist_div.a.get("href"),
+            }
+            for artist_div in artist_divs
+        ]
+    )
 
 
 if __name__ == "__main__":
@@ -59,15 +64,50 @@ if __name__ == "__main__":
     except FileNotFoundError:
         g_df = scrape_all_genres()
 
-    print(f"\nGenres loaded into g_df. Found {len(g_df)} total genres.\n")
+    print("STARTING GAME!")
+    print(f"{len(g_df)} genres loaded..")
+    print(f"Guess genres to gain points..", end="\n\n")
 
-    for i in range(5):
+    points = 0
+    game = True
+    iteration = 0
+    while game:
+        iteration += 1
         random_genre = g_df.iloc[randint(0, len(g_df) - 1)]
 
-        print(f"\ngenre found: {random_genre['genre']}")
-
         artists_df = scrape_genre_artists(random_genre)
-        artists_df.index.name = random_genre['genre']
+        artists_df.index.name = random_genre["genre"]
         random_artist = artists_df.iloc[randint(0, len(artists_df) - 1)]
 
-        print(f"{random_artist['artist']}. Listen: {random_artist['spotify_link']}")
+        print(f"Listen: {random_artist['preview_url']}\n")
+
+        answer = wordsegment.segment(random_genre["genre"])
+        split_guess = set(
+            [x.lower() for x in wordsegment.segment(input("Guess the genre: "))]
+        )
+        split_answer = set([x.lower() for x in answer])
+
+        song_points = 0
+        for guess_part in split_guess:
+            if guess_part in split_answer:
+                song_points += 10
+            elif song_points >= 2:
+                song_points -= 2
+
+        if split_answer == split_guess:
+            song_points = 50
+
+        points += song_points
+
+        print(f"{iteration}. Gained +{song_points} song points. Total score: {points}")
+        print(f"\nThe correct genre is: {' '.join(answer)}.")
+        print(
+            f"Artist: {random_artist['artist']}. Save: {random_artist['spotify_link']}",
+            end="\n\n",
+        )
+        key = input("Any key to continue, press x to quit..\n")
+
+        if key == "x":
+            print(f"Points: {points}. Total guesses: {iteration}")
+            game = False
+            quit()
