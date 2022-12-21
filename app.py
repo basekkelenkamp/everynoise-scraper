8964579b1d934f30f94639e0e0dd3305477e78a4
Bas Ekkelenkamp <basekkelenkamp@MacBook-Pro.local>
2022-12-21 18:39:07 +0100

    Revert "add genre to session"
    This reverts commit 4d68ed7a65ee64b1fb41fd8005aacfc7c07eed8a.

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
-from flask import Flask, session, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect
 from pprint import pprint
 from game import Game
-from flask_session import Session
-
 
 app = Flask(__name__)
 game = Game()
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = 'filesystem'
-Session(app)
 
 
 @app.route("/")
@@ -19,8 +14,7 @@ def index():
 
 @app.route("/start_game", methods=["GET", "POST"])
 def start_game():
-    round_number, points_total, artist_url, random_genre = game.init_new_round()
-    session["genre"] = random_genre
+    round_number, points_total, artist_url = game.init_new_round()
 
     print(f"round: {round_number}, points: {points_total} url: {artist_url}")
     return render_template("game.html", round_number=round_number, points_total=points_total, artist_url=artist_url)
@@ -29,7 +23,7 @@ def start_game():
 @app.route("/genre_guesser", methods=["POST"])
 def genre_guesser():
     guess = request.form.get("genre_guess", "")
-    answer_dict = game.submit_guess(guess, session["genre"])
+    answer_dict = game.submit_guess(guess)
 
     # unpacking: rounds, points, guess, genre, artist, spotify_link
     return render_template("answer.html", **answer_dict)
